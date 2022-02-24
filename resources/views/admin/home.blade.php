e4a4380d0a575e3722d2568214f60d413844a520
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-02-24 19:19:07 +0100

    Inserite info utente nella dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,7 +2,13 @@
 
 @section('content')
     
-    <h1>Benvenuto {{ Auth::user()->name }} nella dashboard</h1>
-    <h4>Phone: {{  }}</h4>
+    <h1>Benvenuto {{ $user->name }} nella dashboard</h1>
+    
+    @if ($userInfo)
+        <h3>I tuoi dati:</h3>
+        <h5>Email: {{ $user->email }}</h5>
+        <h5>Phone: {{ $userInfo->phone }}</h5>
+        <h5>Address: {{ $userInfo->address }}</h5>
+    @endif
 
 @endsection
