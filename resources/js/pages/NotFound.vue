b6ee15b9e59866c3295cc24c48f5e5683e233d0b
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-03-03 17:07:26 +0100

    Aggiunto gestione del components NotFound.vue

---
+++ resources/js/pages/NotFound.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>ERRORE 404</h1>
+        <h2>Non abbiamo trovato ciò che cercavi.<router-link :to="{ name: 'home'}" class="nav-link d-inline-block">Ritorna alla Home</router-link></h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NotFound'
+}
+</script>
