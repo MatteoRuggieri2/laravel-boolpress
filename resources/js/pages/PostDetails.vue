4dd4db052370b6b3026526d097178890c925195d
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-03-04 17:26:15 +0100

    Aggiunto category e tags alla page PostDetails.vue

--- resources/js/pages/PostDetails.vue
+++ resources/js/pages/PostDetails.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="container">
         <h1>{{ post.title }}</h1>
+        <h3 v-if="post.category">Categoria: {{ post.category.name }}</h3>
+        <h5 v-if="post.tags.length > 0">
+            <span>Tags: </span>
+            <a v-for="tag in post.tags" :key="tag.id" href="#" class="badge rounded-pill bg-light text-dark">{{ tag.name }}</a>
+        </h5>
         <p>{{ post.content }}</p>
     </div>
 </template>
